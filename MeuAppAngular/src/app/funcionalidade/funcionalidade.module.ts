83a7177f067c137b7c3939aaaf7cffd48c53c168
Romulo Freitas <rfcontatosvia@gmail.com>
2023-03-21 11:26:55 -0300

    learn: ng g service nome-da-service

---
+++ MeuAppAngular/src/app/funcionalidade/funcionalidade.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+
+@NgModule({
+  declarations: [
+    //componentes
+  ],
+  imports: [
+    //modulos
+    CommonModule
+  ],
+  providers: [
+    //serviços
+  ]
+})
+export class FuncionalidadeModule { }
